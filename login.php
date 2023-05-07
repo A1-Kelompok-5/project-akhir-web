b11e63c0fe85d842bbff30584ac219c149ac1862
Panji Mugni Ramadhani <panjiramadhani60@gmail.com>
2023-05-07 22:36:01 +0800

    baiki sedikit

--- login.php
+++ login.php
@@ -15,21 +15,21 @@ if($data['role']=="PEMILIK") {
     $_SESSION['role']=$data['role'];
     $_SESSION['username']=$data['username'];
     echo "<script>
-        alert('Berhasil Login. ');
+        alert('Berhasil Login.. ');
         document.location.href = 'pemilik/beranda.php';
         </script>";
 }elseif($data['role']=="ADMIN") {
     $_SESSION['role']=$data['role'];
     $_SESSION['username']=$data['username'];
     echo "<script>
-        alert('Berhasil Login. ');
+        alert('Berhasil Login.. ');
         document.location.href = 'admin/beranda.php';
         </script>";
 }elseif($data['role']=="PEMBELI") {
     $_SESSION['role']=$data['role'];
     $_SESSION['username']=$data['username'];
     echo "<script>
-        alert('Berhasil Login. ');
+        alert('Berhasil Login.. ');
         document.location.href = 'pembeli/beranda.php';
         </script>";
 }
