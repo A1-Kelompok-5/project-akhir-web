6206a173e271c0f3ca71652353bbec23f42d6031
Panji Mugni Ramadhani <panjiramadhani60@gmail.com>
2023-05-10 20:53:49 +0800

    database baru

--- gramedia.sql
+++ gramedia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Bulan Mei 2023 pada 11.50
+-- Waktu pembuatan: 10 Bulan Mei 2023 pada 14.51
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -32,40 +32,39 @@ CREATE TABLE `buku` (
   `kategori` varchar(15) NOT NULL,
   `judul` varchar(50) NOT NULL,
   `penulis` varchar(50) NOT NULL,
-  `harga` char(10) NOT NULL,
-  `stock` varchar(5) NOT NULL
+  `harga` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `buku`
 --
 
-INSERT INTO `buku` (`id_buku`, `kategori`, `judul`, `penulis`, `harga`, `stock`) VALUES
-(1, 'ilmu sosial', 'Aldera', 'Teddy Wibisana', '84000', '23'),
-(2, 'ilmu sosial', 'Cahaya Baru', 'Buya Hamka', '72000', '12'),
-(3, 'ilmu sosial', 'Jalan Pulang', 'Kepustakaan Populer Grame', '93000', '33'),
-(4, 'ilmu sosial', 'How to Grow Old', 'Marcus Tullius Cicero', '67000', '3'),
-(5, 'ilmu sosial', 'Memburu Cipta,Rasa & Karsa', 'Mayjend (Purn) Samsudin', '101000', '27'),
-(6, 'kesehatan', 'Keperawatan Jiwa', 'Ns. Sutejo, M.Kep., Sp.Kep.J.', '54000', '2'),
-(7, 'kesehatan', 'Komunikasi Dalam Keperawatan', 'Tri Prabowo, S.kp., M.sc.', '35000', '6'),
-(8, 'kesehatan', 'Ilmu Kesehatan Masyarakat', 'I Ketut Swarjana', '108000', '32'),
-(9, 'kesehatan', 'Advokasi', 'Hadi Pratomo', '149000', '19'),
-(10, 'kesehatan', 'Pendidikan Keperawatan Gerontik', 'Abdul Muhith', '103000', '13'),
-(11, 'sains', 'Gen', 'Siddhartha Mukherjee', '149000', '7'),
-(12, 'sains', 'The Selfish Gene', 'Richard Dawkins', '71000', '3'),
-(13, 'sains', 'The Biology of Belief', 'Bruce Lipton ', '102000', '12'),
-(14, 'sains', 'Biologi Molekular', 'Triwibowo Yuwono ', '152000', '9'),
-(15, 'sains', 'Morfologi Tumbuhan', 'Dewi Rosanti ', '238000', '13'),
-(16, 'astronomi', 'Kosmos: Aneka Ragam Dunia', 'Ann Druyan ', '108000', '43'),
-(17, 'astronomi', 'Pokok-Pokok Materi Statistik 1 edisi 2', 'Iqbal Hasan ', '122000', '31'),
-(18, 'romance', '5 Cm', 'Donny Dhirgantoro ', '67000', '11'),
-(19, 'romance', 'Ananta Prahadi', 'Risa Saraswati ', '71000', '11'),
-(20, 'romance', 'Merayakan Kehilangan', 'Brian Khrisna ', '58000', '6'),
-(21, 'romance', 'Novel Berjuta Rasanya', 'Tere Liye', '50000', '6'),
-(22, 'novel', 'EL', 'Luluk HF ', '99000', '5'),
-(23, 'novel', 'Taman Sang Nabi', ' Kahlil Gibran ', '35000', '12'),
-(24, 'novel', 'Komet', ' Tere Liye ', '71000', '11'),
-(25, 'novel', 'Novel Dilan 2:Dia Adalah Dilanku Tahun 1991', ' Pidi Baiq ', '89000', '44');
+INSERT INTO `buku` (`id_buku`, `kategori`, `judul`, `penulis`, `harga`) VALUES
+(1, 'ilmu sosial', 'Aldera', 'Teddy Wibisana', '84000'),
+(2, 'ilmu sosial', 'Cahaya Baru', 'Buya Hamka', '72000'),
+(3, 'ilmu sosial', 'Jalan Pulang', 'Kepustakaan Populer Grame', '93000'),
+(4, 'ilmu sosial', 'How to Grow Old', 'Marcus Tullius Cicero', '67000'),
+(5, 'ilmu sosial', 'Memburu Cipta,Rasa & Karsa', 'Mayjend (Purn) Samsudin', '101000'),
+(6, 'kesehatan', 'Keperawatan Jiwa', 'Ns. Sutejo, M.Kep., Sp.Kep.J.', '54000'),
+(7, 'kesehatan', 'Komunikasi Dalam Keperawatan', 'Tri Prabowo, S.kp., M.sc.', '35000'),
+(8, 'kesehatan', 'Ilmu Kesehatan Masyarakat', 'I Ketut Swarjana', '108000'),
+(9, 'kesehatan', 'Advokasi', 'Hadi Pratomo', '149000'),
+(10, 'kesehatan', 'Pendidikan Keperawatan Gerontik', 'Abdul Muhith', '103000'),
+(11, 'sains', 'Gen', 'Siddhartha Mukherjee', '149000'),
+(12, 'sains', 'The Selfish Gene', 'Richard Dawkins', '71000'),
+(13, 'sains', 'The Biology of Belief', 'Bruce Lipton ', '102000'),
+(14, 'sains', 'Biologi Molekular', 'Triwibowo Yuwono ', '152000'),
+(15, 'sains', 'Morfologi Tumbuhan', 'Dewi Rosanti ', '238000'),
+(16, 'astronomi', 'Kosmos: Aneka Ragam Dunia', 'Ann Druyan ', '108000'),
+(17, 'astronomi', 'Pokok-Pokok Materi Statistik 1 edisi 2', 'Iqbal Hasan ', '122000'),
+(18, 'romance', '5 Cm', 'Donny Dhirgantoro ', '67000'),
+(19, 'romance', 'Ananta Prahadi', 'Risa Saraswati ', '71000'),
+(20, 'romance', 'Merayakan Kehilangan', 'Brian Khrisna ', '58000'),
+(21, 'romance', 'Novel Berjuta Rasanya', 'Tere Liye', '50000'),
+(22, 'novel', 'EL', 'Luluk HF ', '99000'),
+(23, 'novel', 'Taman Sang Nabi', ' Kahlil Gibran ', '35000'),
+(24, 'novel', 'Komet', ' Tere Liye ', '71000'),
+(25, 'novel', 'Novel Dilan 2:Dia Adalah Dilanku Tahun 1991', ' Pidi Baiq ', '89000');
 
 -- --------------------------------------------------------
 
@@ -109,7 +108,10 @@ CREATE TABLE `riwayat_transaksi` (
 INSERT INTO `riwayat_transaksi` (`id_riwayat`, `id`, `tanggal`, `status`) VALUES
 (1, 1, '2023-04-29', 'BERHASIL'),
 (2, 2, '2023-04-29', 'GAGAL'),
-(35, 15, '2023-05-10', 'BERHASIL');
+(35, 15, '2023-05-10', 'BERHASIL'),
+(36, 19, '2023-05-10', 'BERHASIL'),
+(37, 20, '2023-05-10', 'BERHASIL'),
+(38, 21, '2023-05-10', 'BERHASIL');
 
 -- --------------------------------------------------------
 
@@ -134,7 +136,10 @@ INSERT INTO `transaksi` (`id`, `id_user`, `id_buku`, `jumlah`, `total`) VALUES
 (2, 6, 25, '3', '300000'),
 (21, 5, 2, '2', '144000'),
 (22, 5, 1, '1', '84000'),
-(23, 5, 5, '1', '101000');
+(23, 5, 5, '1', '101000'),
+(24, 5, 1, '5', '420000'),
+(25, 5, 1, '4', '336000'),
+(26, 5, 1, '7', '588000');
 
 -- --------------------------------------------------------
 
@@ -217,19 +222,19 @@ ALTER TABLE `buku`
 -- AUTO_INCREMENT untuk tabel `keranjang`
 --
 ALTER TABLE `keranjang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT untuk tabel `riwayat_transaksi`
 --
 ALTER TABLE `riwayat_transaksi`
-  MODIFY `id_riwayat` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_riwayat` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
